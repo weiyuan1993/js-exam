09d8dcbb3cad57de38565ae6764a0c5faeb2ed5b
nchang <neil.chang@innovasolutions.com>
2017-06-07 13:29:09 +0800

    Add CodeMirror

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import CodeMirror from 'react-codemirror';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="editor">
+          <CodeMirror options={{
+            mode: 'javascript',
+            lineNumbers: true,
+          }} />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="result" />
       </div>
     );
   }
