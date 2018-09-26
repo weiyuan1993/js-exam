d06051eeebdb95d0eacf71704beeef2aea2db8c1
jeno5980515 <jeno5980515@yahoo.com.tw>
2018-09-26 15:12:22 +0800

    remove firebase

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
-import authCheck from "./utils/authCheck";
 
-import * as firebase from "firebase";
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
@@ -21,28 +19,13 @@ const store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyC7Z6ltGrL4xJ7cM4EiDRuG7yYU74OWti8",
-  authDomain: "innova-ppol.firebaseapp.com",
-  databaseURL: "https://innova-ppol.firebaseio.com",
-  projectId: "innova-ppol",
-  storageBucket: "innova-ppol.appspot.com",
-  messagingSenderId: "38306620670"
-};
-const firebaseApp = firebase.initializeApp(config);
-store.dispatch(
-  require("./actions/firebase").initFirebase(firebaseApp)
-);
-
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
-const AppWithAuth = authCheck(App);
 ReactDOM.render(
   <Provider store={store}>
     <MuiThemeProvider>
-      <AppWithAuth />
+      <App />
     </MuiThemeProvider>
   </Provider>,
   document.getElementById("root")
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import code from './code';
-import firebase from './firebase';
 
 const reducer = combineReducers({
-  code,
-  firebase
+  code
 });
 
 export default reducer;
