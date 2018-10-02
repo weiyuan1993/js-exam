766756822e41d46d6d6457787e21c0b76395051a
jeno5980515 <jeno5980515@yahoo.com.tw>
2018-10-02 16:46:05 +0800

    modify authLogin bug

--- src/utils/authLogin.js
+++ src/utils/authLogin.js
@@ -1,7 +1,7 @@
 import md5 from 'blueimp-md5';
 import moment from 'moment';
 
-const authLogin = (s) => `${parseInt(md5(moment().utc().format('YYYY-MM-DD HH')).slice(0, 10))}`.slice(0, 4);
+const authLogin = (s) => `${parseInt(md5(moment().utc().format('YYYY-MM-DD HH')).slice(0, 10))}`.slice(0, 4) === s;
 
 export {
   authLogin
