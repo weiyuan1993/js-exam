1e909bc55a7285aa4805c9f44dce1a8baf5e2a5c
Wendy Lin <Wendy.Lin@tdc.Innovasolutions.int>
2019-01-10 11:12:43 +0800

    Fixed error

--- src/pages/PlayBackPage/index.js
+++ src/pages/PlayBackPage/index.js
@@ -5,13 +5,11 @@ import { graphqlOperation } from 'aws-amplify';
 import { getTest } from 'graphql/queries';
 import { onCreateRoom } from 'graphql/subscriptions';
 
-import CommentBox from 'components/CommentBox';
 import CreateRoomView from 'containers/CreateRoomView';
 
 const PlayBakPge = props => (
   <div>
     <div>
-      {/* TODO: Room list with with lazy-loading next dataset. Here we load 1000 rooms instead. */}
       <Connect
         query={graphqlOperation(getTest, {
           id: 'a2bd44d0-30a0-481c-997e-1e05972153f6',
@@ -22,12 +20,9 @@ const PlayBakPge = props => (
           console.log(test, '2390ruewfihodln');
           if (error) return <h3>Error</h3>;
           if (loading || !test) return <h3>Loading...</h3>;
-          return <div>2-w9ur0hgionfdkvled</div>
+          return <div>2-w9ur0hgionfdkvled</div>;
         }}
       </Connect>
-    </div>
-    <div>
-      <CommentBox {...props} />
     </div>
   </div>
 );
