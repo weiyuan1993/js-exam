f13b2622547d9abdc62739d0b3664923adbd1fc4
hankkuo7444 <hank7444@gmail.com>
2019-01-17 16:31:14 +0800

    refactor: remove lagency code, again

--- src/reducers.js
+++ src/reducers.js
@@ -5,11 +5,8 @@
 import { combineReducers } from 'redux';
 import formReducer from 'redux-form/es/reducer';
 
-import javascript from 'models/javascript/reducer';
-import react from 'models/react/reducer';
 import login from 'models/login/reducer';
-import category from 'models/category/reducer';
-import _console from 'models/console/reducer';
+
 import room from 'models/room/reducer';
 import record from 'models/record/reducer';
 import question from 'models/question/reducer';
@@ -19,11 +16,7 @@ import question from 'models/question/reducer';
  */
 export default function createReducer(injectedReducers = {}) {
   return combineReducers({
-    javascript,
-    react,
     login,
-    category,
-    console: _console,
     room,
     record,
     question,
